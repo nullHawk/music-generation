ab39a55a5c906116d0ae6c02a5ddccba7cedc6cc
Suryansh <suryanshsinghshakya1@gmail.com>
2024-11-24 11:33:32 +0000

    added model.py

--- model.py
+++ model.py
@@ -2,5 +2,18 @@ import torch
 import torch.nn as nn
 
 class MusicGenerationLSTM(nn.Module):
-    def __init__(self, input_size,  hidden_size, n_layers):
-        super(MusicGenerationLSTM, self).__init__()
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(MusicGenerationLSTM).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
